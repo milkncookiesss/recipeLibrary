33469dba746b8322d7411bd69841b1adde851fd0
Peter Park <p.parkcontact@gmail.com>
2023-07-20 14:45:31 -0700

    "created base server, added route files"

---
+++ server/routes.js
@@ -0,0 +1,14 @@
+import express from "express";
+// const controller = require('./controllers.js');
+const router = express.Router();
+// router
+//   .route('/pokemon')
+//   .get(controller.pokemon.get);
+
+// router
+//   .route('/user')
+//   .get(controller.user.get)
+//   .post(controller.user.post)
+//   .delete(controller.user.delete);
+
+export default router;
